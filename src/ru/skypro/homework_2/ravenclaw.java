7e3ddfcdc260aa359c649ee4ca75753ead16ad51
Aleksandr Malashenko <alex.08.1995@gmail.com>
2022-05-24 15:21:39 +1000

    Homework done

--- src/ru/skypro/homework_2/ravenclaw.java
+++ src/ru/skypro/homework_2/ravenclaw.java
@@ -1,4 +1,4 @@
-package ru.skypro;
+package ru.skypro.homework_2;
 
 public class ravenclaw extends hogwarts{
     private int mind;       //ум
---
+++ src/ru/skypro/homework_3/ServiceStation.java
@@ -0,0 +1,28 @@
+package ru.skypro.homework_3;
+
+public class ServiceStation {
+    public static void check(Car car) {
+        System.out.println("Обслуживаем " + car.getModelName());
+        for (int i = 0; i < car.getWheelsCount(); i++) {
+            car.updateTyre();
+        }
+        car.checkEngine();
+    }
+
+    public static void check(Bicycle bicycle) {
+        System.out.println("Обслуживаем " + bicycle.getModelName());
+        for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+            bicycle.updateTyre();
+        }
+    }
+
+    public static void check(Truck truck) {
+        System.out.println("Обслуживаем " + truck.getModelName());
+        for (int i = 0; i < truck.getWheelsCount(); i++) {
+            truck.updateTyre();
+        }
+        truck.checkEngine();
+        truck.checkTrailer();
+    }
+
+}
